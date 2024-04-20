923f439dfeb37de299d911c48e59d5af18f2fe6c
Tom Childress <childressiv.dev@gmail.com>
2024-04-20 12:42:18 -0700

    Comments about app design in page.tsx

--- ideavote-app/src/app/page.tsx
+++ ideavote-app/src/app/page.tsx
@@ -3,3 +3,26 @@ export default function Home() {
     <h1>Welcome! Vote an idea</h1>
   );
 }
+
+{/*
+* [ ] - add idea
+      [ ] - edit 
+      [ ] - delete
+
+  [ ] - add description
+      [ ] - edit 
+      [ ] - delete
+
+  [ ] - comments on a ideas
+  
+  [ ] - link to share your ideas page
+
+  [ ] - Auth0 
+      [ ] - connect GitHub account
+
+  [ ] - upvote 
+      [ ] - counter
+      [ ] - green arrow
+      [ ] - one vote per project 
+
+*/}
